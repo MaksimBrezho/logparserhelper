0f8ad1ed88b227c5f30968af660fab5d6c726796
Maksim Brezho <70651566+MaksimBrezho@users.noreply.github.com>
2025-06-07 09:01:32 +0300

    Add window icon

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 from gui.app_window import AppWindow
+import os
 
 def main():
     root = tk.Tk()
     root.title("LogParserHelper")
     root.geometry("1000x700")
+
+    icon_path = os.path.join(os.path.dirname(__file__), "icon", "ALLtoCEF.ico")
+    if os.path.exists(icon_path):
+        try:
+            root.iconbitmap(icon_path)
+        except Exception:
+            pass
 
     app = AppWindow(master=root)
     app.pack(fill="both", expand=True)
